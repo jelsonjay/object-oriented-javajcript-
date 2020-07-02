05d80fec7ecc229d19a4412fd7fae2f965dfac4d
jelson <jelson20@outlook.com>
2020-07-02 21:08:20 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,6 +4,17 @@ class Employees {
 		this.name = name;
 		this.email = email;
 		this.job_title = job_title;
+		this.total = 0;
+	}
+	signin() {
+		console.log(this.email, 'login now!');
+	}
+	signout() {
+		console.log(this.email, 'im loged out!');
+	}
+	updateTotal() {
+		this.total++;
+		console.log(this.email, 'the total is', this.total);
 	}
 }
 
@@ -11,5 +22,8 @@ class Employees {
 let developer = new Employees('Andre', 'andre@gmail.com', 'Developer');
 let tester = new Employees('Ria Turner', 'ria25@gmail.com', 'Tester');
 
-console.log(developer);
-console.log(tester);
+//console.log(developer);
+//console.log(tester);
+
+//developer.signin();
+//tester.signout();
