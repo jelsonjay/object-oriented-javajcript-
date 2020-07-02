70a50edcfcde8cdae6eff841f2e49db5229aa11b
jelson <jelson20@outlook.com>
2020-07-02 20:36:00 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
 'use strict';
+class Employees {
+	constructor(name, email, job_title) {
+		this.name = name;
+		this.email = email;
+		this.job_title = job_title;
+	}
+}
+
+// create new object
+let developer = new Employees('Andre', 'andre@gmail.com', 'Developer');
+let tester = new Employees('Ria Turner', 'ria25@gmail.com', 'Tester');
+
+console.log(developer);
+console.log(tester);
